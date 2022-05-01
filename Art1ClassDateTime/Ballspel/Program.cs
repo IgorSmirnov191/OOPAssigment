6a32941b0ddd75633c63ecb51506d04d84ff42b5
igor.smirnov@telenet.be <igor.smirnov@telenet.be>
2022-05-01 22:57:10 +0200

    added ballspel

---
+++ Art1ClassDateTime/Ballspel/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ballspel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.WindowHeight = 20;
+            Console.WindowWidth = 30;
+            Ball b1 = new Ball(4, 4, 1, 0);
+            PlayerBall player = new PlayerBall(10, 10, 0, 0);
+            while (true)
+            {
+                Console.Clear();
+                //Ball
+                b1.Update();
+                b1.Draw();
+                //SpelerBall
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
+                    player.ChangeVelocity(key);
+                }
+                player.Update();
+                player.Draw();
+                //Check collisions
+                if (Ball.CheckHit(b1, player))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Gewonnen!");
+                    Console.ReadLine();
+                }
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+}
