4d03cf807dc8b83f9aa410e6f6af1a1e3664f2ba
Igor <igor.smirnov@telenet.be>
2022-05-10 00:15:05 +0200

    added employee Liskov project

---
+++ Art1ClassDateTime/LiskovSubstitutionPrinciple/ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IManager accountingVP = new CEO();
+            accountingVP.FirstName = "Emma";
+            accountingVP.LastName = "Stone";
+            accountingVP.CalculatePerHourRate(4);
+
+            Employee emp = new Employee();
+            emp.FirstName = "Tim";
+            emp.LastName = "Corey";
+            emp.AssingManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{emp.FirstName}'s salary is e{emp.Salary } /hour.");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Art1ClassDateTime/LiskovSubstitutionPrinciple/DemoLibrary/DemoLibrary/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DemoLibrary
+{
+    public class Employee : BaseEmployee, IManaged
+    {
+        public IEmployee Manager { get; set; } = null;
+       
+        public virtual void AssingManager(IEmployee manager)
+        {
+            // Simulate doing other tasks here - otherwise, this should be
+            // a property set statement, not a method.
+            Manager = manager;
+        }
+
+    }
+}
