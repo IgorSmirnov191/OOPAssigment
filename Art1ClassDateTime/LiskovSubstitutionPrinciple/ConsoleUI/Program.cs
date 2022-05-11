97f4f7e410da563c72cf01db545de0190fd3de5b
Igor <igor.smirnov@telenet.be>
2022-05-11 11:47:22 +0200

    added uml diagram

--- Art1ClassDateTime/LiskovSubstitutionPrinciple/ConsoleUI/Program.cs
+++ Art1ClassDateTime/LiskovSubstitutionPrinciple/ConsoleUI/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleUI
             accountingVP.LastName = "Stone";
             accountingVP.CalculatePerHourRate(4);
 
-            Employee emp = new Employee();
+            Employee emp = new Manager();
             emp.FirstName = "Tim";
             emp.LastName = "Corey";
             emp.AssingManager(accountingVP);
