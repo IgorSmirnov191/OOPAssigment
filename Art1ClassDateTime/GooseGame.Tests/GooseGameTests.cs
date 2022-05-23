ede749bf5811f031e7e6e21aebfac78d76e8d9f3
Igor <igor.smirnov@telenet.be>
2022-05-23 15:57:04 +0200

    fixed xunit test

---
+++ Art1ClassDateTime/GooseGame.Tests/GooseGameTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using GooseGame;
+
+namespace GooseGame.Tests
+{
+    public class GooseGameTests
+    {
+        [Fact]
+        public void MakeTurn_NextSpaceShouldCalculate()
+        {
+            // Arrange
+            ISpace expected = new StaticSpace(5, null);
+
+            // Act
+            Board board = new Board(new SpacePack(63));
+            Piece piece = new Piece(new Player("P1", "Password"));
+            Game game = new Game("GooseBoard", board);
+            game.ActiveBoard.Pieces.Add(piece);
+            board.Sequenser.Board.DiceRoller.Scoores.Add(1);
+            board.Sequenser.Board.DiceRoller.Scoores.Add(3);
+            board.Sequenser.SpaceForward = 4;
+            ISpace actual = board.Sequenser.MakeTurn(piece);
+
+            // Assert
+            Assert.Equal(expected.Index, actual.Index);
+
+        }
+    }
+}
